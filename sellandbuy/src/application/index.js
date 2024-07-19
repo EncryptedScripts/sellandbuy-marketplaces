aa4c6df8fbf7cfa52ae84eddbf51cc04156a8273
EncryptedScripts <44745889+EncryptedScripts@users.noreply.github.com>
2024-07-19 22:07:14 +0700

    new

--- sellandbuy/src/application/index.js
+++ sellandbuy/src/application/index.js
@@ -1,7 +1,31 @@
+require("dotenv").config();
 const express = require("express");
+const session = require("express-session");
 const path = require("path");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const layouts = require("express-ejs-layouts");
+const mongoSanitize = require("express-mongo-sanitize");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
+const publicDirectoryPath = path.join(__dirname, "../../public");
+const viewsPath = path.join(__dirname, "../templates");
+
+mongoose.connect("mongodb://127.0.0.1:27017/sellandbuy", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+app.set("view engine", "ejs");
+app.set("views", viewsPath);
+app.use(express.static(publicDirectoryPath));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
